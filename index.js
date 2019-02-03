c287dd4fcf94354a826c01dab22f9a10cd63debd
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-03 11:22:47 -0800

    Added validAnagram

--- index.js
+++ index.js
@@ -64,12 +64,12 @@ function betterSquared(arr1, arr2) {
     // object lookups happen in constant time
     for (let key in obj1) {
         // check to see if the key squared exist in object 2
-        if (!(key **2 in obj2)) {
+        if (!(key ** 2 in obj2)) {
             return false;
         }
 
         // check if the count of key and key^2 are the same
-        if (obj1[key] !== obj2[key ** 1]) {
+        if (obj1[key] !== obj2[key ** 2]) {
             return false;
         }
      
@@ -81,3 +81,51 @@ function betterSquared(arr1, arr2) {
 
 console.log(betterSquared([1,2,3], [1,4,9])); // true
 console.log(betterSquared([1,2,3], [1,4,10])); // false
+
+
+// Frequency Counter  - validAnagram
+// Given two strings, write a function to determine if the second string is an agagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+// Examples:
+// 1 validAnagram('','') // true
+// 2 validAnagram('aaz', 'zza') // false
+// 3 validAnagram('anagram', 'nagaram') // true
+// 4 validAnagram('rat', 'car') // false
+// 5 validAnagram('awesome', 'awesom') //false
+// 6 validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') //false
+// 7 validAnagram('qwerty', 'qeywrt') // true
+// 8 validAnagram('texttwisttime', 'timetwisttext') // true
+
+function validAnagram(first, second) {
+    if (first.length !== second.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        // if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        // can't find letter or letter is zero then it's not an anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -=1;
+        }
+    }
+
+    return true;
+}
+
+console.log(validAnagram('','') + ' should be true'); // true
+console.log(validAnagram('aaz', 'zza') + ' should be false'); // false
+console.log(validAnagram('anagram', 'nagaram') + ' should be true'); // true
+console.log(validAnagram('rat', 'car') + ' should be false'); // false
+console.log(validAnagram('awesome', 'awesom') + ' should be false'); //false
+console.log(validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') + ' should be false'); //false
+console.log(validAnagram('qwerty', 'qeywrt') + ' should be true'); // true
+console.log(validAnagram('texttwisttime', 'timetwisttext') + ' should be true'); // true
