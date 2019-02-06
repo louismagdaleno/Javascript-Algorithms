7fb9c374539b5aedb8b7c6dcf88aac55578c23c5
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-06 09:50:00 -0800

    Added anagrams.js

---
+++ algorithms/anagrams.js
@@ -0,0 +1,7 @@
+// write a function isAnagram that accepts two arrays as parameters and checks to see if the second array is an anagram of the first.
+// return true or false
+
+// Naive solution
+function isAnagram (arr1, arr2) {
+    
+}
