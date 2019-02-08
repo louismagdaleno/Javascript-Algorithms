f9b71602b2d887502a4c47bf85678151bca6c6fb
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-08 12:03:45 -0800

    Completed inefficient maxSubarraySum algorithm

--- algorithms/sliding_window.js
+++ algorithms/sliding_window.js
@@ -11,7 +11,20 @@
 // Write a function called maxSubarraySum which accepts an array of integers and a number called n.
 // The function should calculate the maximum sum of n consecutive elements in the array.
 
+// O(n^2) time complexity due to nested for loops.
+function maxSubarraySum(arr, n) {
+    if (arr.length === 0) return null;
+    let max = -Infinity;
+    for (let i = 0; i < arr.length; i++) {
 
+        let sum = arr[i];
+        for (let j = i + 1; j < i + n; j ++) {
+            sum += arr[j];
+        }
+       if (sum > max) max = sum;
+    }
+
+}
 maxSubarraySum([1,2,5,2,8,1,5],2) // 10
 maxSubarraySum([1,2,5,2,8,1,5],4) // 17
 maxSubarraySum([4,2,1,6],1) // 6
