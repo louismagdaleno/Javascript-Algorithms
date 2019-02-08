c66dddd645673cf80edf3b9cacdf1cd4fc65e2da
Louis Magdaleno <buildthatapp@gmail.com>
2019-02-08 12:40:21 -0800

    Refactored maxSubarraySum

--- algorithms/sliding_window.js
+++ algorithms/sliding_window.js
@@ -11,22 +11,32 @@
 // Write a function called maxSubarraySum which accepts an array of integers and a number called n.
 // The function should calculate the maximum sum of n consecutive elements in the array.
 
+// maxSubarraySum naive solution
 // O(n^2) time complexity due to nested for loops.
-function maxSubarraySum(arr, n) {
-    if (arr.length === 0) return null;
-    let max = -Infinity;
-    for (let i = 0; i < arr.length - n + 1; i++) {
-
-        let sum = arr[i];
-        for (let j = 0; j < n; j ++) {
-            sum += arr[j];
-        }
-       if (sum > max) max = sum;
+function maxSubarraySum(arr, num) {
+    if ( num > arr.length){
+      return null;
     }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i ++){
+      temp = 0;
+      for (let j = 0; j < num; j++){
+        temp += arr[i + j];
+      }
+      if (temp > max) {
+        max = temp;
+      }
+    }
+    return max;
+  }
+console.log(maxSubarraySum([1,2,5,2,8,1,5],2)) // 10
+console.log(maxSubarraySum([1,2,5,2,8,1,5],4)) // 17
+console.log(maxSubarraySum([4,2,1,6],1)) // 6
+console.log(maxSubarraySum([4,2,1,6,2],4)) // 13
+console.log(maxSubarraySum([],4)) // null
+
+// maxSubarraySum efficient solution
+// O(n) time complexity
+function maxSubarraySumPlus(arr,n) {
 
 }
-maxSubarraySum([1,2,5,2,8,1,5],2) // 10
-maxSubarraySum([1,2,5,2,8,1,5],4) // 17
-maxSubarraySum([4,2,1,6],1) // 6
-maxSubarraySum([4,2,1,6,2],4) // 13
-maxSubarraySum([],4) // null
